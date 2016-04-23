d169433c13e92d32d0aafdab526c117f5d4cf337
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-23 15:18:00 +0530

    Add module app

---
+++ src/app/app.module.js
@@ -0,0 +1,12 @@
+// Immediately Invoked Function Expression (IIFE)
+(function() {
+    'use strict';
+
+    angular
+        .module('app', [
+            // Angular modules
+            'ngRoute',
+            // third-party modules
+            'firebase'
+        ]);
+})();
