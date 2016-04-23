7603f56d49c628f54aa4f7847bef0712b781cbfe
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-23 23:01:55 +0530

    Add Firebase and created addParty function

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -5,10 +5,28 @@
         .module('app.waitList')
         .controller('WaitListController', WaitListController);
 
-    function WaitListController() {
+    WaitListController.$inject = ['$firebaseArray'];
+
+    function WaitListController($firebaseArray) {
         // ViewModel
         var vm = this;
+        var fireParties = new Firebase('https://blistering-torch-6934.firebaseio.com/parties');
 
-        vm.parties = [1, 2, 3, 4];
+        vm.newParty = new Party();
+        vm.parties = $firebaseArray(fireParties);
+        vm.addParty = addParty;
+
+        function Party() {
+            this.name = '';
+            this.phone = '';
+            this.size = '';
+            this.done = false;
+            this.notified = false;
+        }
+
+        function addParty() {
+            vm.parties.$add(vm.newParty);
+            vm.newParty = new Party();
+        }
     }
 })();
