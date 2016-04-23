38b34ca9c9d7e70c97d40592371da7403217d921
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-23 16:16:49 +0530

    Add landing.module and config.route

---
+++ src/app/landing/config.route.js
@@ -0,0 +1,16 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('app.landing')
+        .config(configFunction) // Going to execute the config on the module app.landing
+
+    configFunction.$inject = ['$routeProvider'];
+
+    function configFunction($routeProvider) {
+        $routeProvider
+            .when('/', {
+                templateUrl: 'app/landing/landing.html'
+            });
+    }
+})();
