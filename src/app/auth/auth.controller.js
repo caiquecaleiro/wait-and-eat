003c550d88635a6cd2ab2001eba871d682c97460
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-24 21:30:42 +0530

    Add firebaseAuth and register function to AuthController

--- src/app/auth/auth.controller.js
+++ src/app/auth/auth.controller.js
@@ -3,9 +3,24 @@
 
     angular
         .module('app.auth')
-        .controller('AuthController', authController);
+        .controller('AuthController', AuthController);
 
-    function authController() {
-        
+    AuthController.$inject = ['$firebaseAuth'];
+
+    function AuthController($firebaseAuth) {
+        var vm = this;
+        var firebaseReference = new Firebase('https://blistering-torch-6934.firebaseio.com/parties');
+        var firebaseAuthObject = $firebaseAuth(firebaseReference);
+
+        vm.user = {
+            email: '',
+            password: ''
+        };
+
+        vm.register = register;
+
+        function register(user) {
+            return firebaseAuthObject.$createUser(user);
+        }
     }
 })();
