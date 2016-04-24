531aa207cb42590ab7087185240c6b573a6b3c54
Caique Caleiro <caiquecaleiro@gmail.com>
2016-04-24 17:47:52 +0530

    Add sendTextMessage function

--- src/app/waitList/waitList.controller.js
+++ src/app/waitList/waitList.controller.js
@@ -11,11 +11,13 @@
         // ViewModel
         var vm = this;
         var fireParties = new Firebase('https://blistering-torch-6934.firebaseio.com/parties');
+        var fireTextMessages = new Firebase('https://blistering-torch-6934.firebaseio.com/textMessages');
 
         vm.newParty = new Party();
         vm.parties = $firebaseArray(fireParties);
         vm.addParty = addParty;
         vm.removeParty = removeParty;
+        vm.sendTextMessage = sendTextMessage;
 
         function Party() {
             this.name = '';
@@ -32,6 +34,24 @@
 
         function removeParty(party) {
             vm.parties.$remove(party);
+        }
+
+        /**
+         * This method will create a new text message and save it on Firebase.
+         * After saving it, the node.js server will listen and send a message
+         * for the saved phone number (using the Twilio SMS).
+         *
+         * @param {object} party - The party object.
+         */
+        function sendTextMessage(party) {
+            var newTextMessage = {
+                name: party.name,
+                phoneNumber: party.phone,
+                size: party.size
+            };
+            fireTextMessages.push(newTextMessage);
+            party.notified = true;
+            vm.parties.$save(party);
         }
     }
 })();
